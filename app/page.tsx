3c1085faaeaf18933471b7640ae8106f9bab35ff
Freddie Ley <freddie.ley@icloud.com>
2026-09-15 19:01:20 +0100

    Build patch

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
 import { BluoApp } from '@/components/BluoApp';
-import { TimetableOCR } from '@/components/TimetableOCR';
 
-export default function Home(){ return <><BluoApp/><TimetableOCR/></>; }
+export default function Home(){ return <BluoApp/>; }
